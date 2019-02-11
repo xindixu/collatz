512ec3a5f09c3953d4b71ebf1161a543ee256d7c
Xindi Xu <xindixu@utexas.edu>
2019-02-11 13:46:53 -0600

    generate cache

--- GenerateCache.py
+++ GenerateCache.py
@@ -4,14 +4,16 @@ import json
 cache = []
 
 def generate_input():
+    # cache structure: [max_cyc_len,num_gen_max_cyc]
+    # index 0, 1 for [1-1000]; index 2, 3 for [1001,2000]
     for i in range(1,1000000-1000,1000):
-        (max_cyc_len, num_gen_max_cyc) = collatz_eval_cache(i,i+1000);
-        cache.append((max_cyc_len, num_gen_max_cyc))
+        (max_cyc_len, num_gen_max_cyc) = collatz_eval_cache(i,i+999);
+        cache.append(max_cyc_len)
+        cache.append(num_gen_max_cyc)
 
 def write():
     with open('cache.txt','w') as file:
         json.dump(cache,file)
-
 
 
 if __name__ == "__main__":
